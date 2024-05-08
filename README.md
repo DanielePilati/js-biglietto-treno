71eca49f8a9ff651f4fbf0f3166848b560714529
DanielePilati <danielepdj@gmail.com>
2024-05-08 15:21:18 +0200

    [upd]to readme from js

--- README.md
+++ README.md
@@ -7,7 +7,10 @@ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percor
 - prompt let distance 
 - prompt let userAge
 
+
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+- verifico che vengano inseriti numeri
 
 - il prezzo del biglietto è definito in base ai km (0.21 € al km) 
 
---
+++ main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let distance = Number(prompt('inserisci km da percorrere'));
+let age = parseInt(Number(prompt('inserisci la tua età')));
+const rate = 0.21;
+
+console.log(distance);
+console.log(age);
+
+if (!isNaN(age) && !isNaN(distance)) {
+    let price = distance * rate; 
+    console.log(`per ${distance} km il costo base del biglietto è di ${price} Euro`);
+
+    if (age < 18) {
+        price = price / 1.20;
+        console.log('sei minorenne e hai diritto al 20% di sconto');
+
+    } else if (age > 65) {
+        price = price / 1.40;
+        console.log('sei over 65 e hai diritto al 40% di sconto');
+    } else {
+        console.log('non hai diritto a nessun tipo di sconto');
+    }
+    console.log(`l'importo finale del biglietto è di ${price.toFixed(2)} Euro`); 
+    
+} else {
+    console.log('inserisci numeri');
+}
+
+
+
