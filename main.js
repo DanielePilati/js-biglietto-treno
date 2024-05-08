8d8656bd8ce46a7e36735a38a1027a697884b1f1
DanielePilati <danielepdj@gmail.com>
2024-05-08 15:58:59 +0200

    [fix] fixed lines

--- main.js
+++ main.js
@@ -10,11 +10,9 @@ console.log(age);
 if (!isNaN(age) && !isNaN(distance)) {
     let price = distance * rate; 
     console.log(`per ${distance} km il costo base del biglietto è di ${price.toFixed(2)} Euro`);
-
     if (age < 18) {
         price = price / 1.20;
         console.log('sei minorenne e hai diritto al 20% di sconto');
-
     } else if (age > 65) {
         price = price / 1.40;
         console.log('sei over 65 e hai diritto al 40% di sconto');
@@ -22,8 +20,7 @@ if (!isNaN(age) && !isNaN(distance)) {
         console.log('non hai diritto a nessun tipo di sconto');
     }
     console.log(`l'importo finale del biglietto è di ${price.toFixed(2)} Euro`); 
-    document.getElementById('final-price').innerHTML = `il prezzo del tuo biglietto è di ${price.toFixed(2)} Euro`;
-    
+    document.getElementById('final-price').innerHTML = `il prezzo del tuo biglietto è di ${price.toFixed(2)} Euro`;  
 } else {
     console.log('inserisci numeri');
     document.getElementById('final-price').innerHTML = `Non hai inserito i dati correttamente, riprova.`;
